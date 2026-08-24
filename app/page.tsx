5861ae12fed32cd40537f2a6e848c19622100356
Amit Kavathekar <amitkavathekar123@gmail.com>
2026-08-24 15:14:47 +0530

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ export default function Page() {
           <p>You may now add components and start building.</p>
           <p>We&apos;ve already added the button component for you.</p>
           <Button className="mt-2">Button</Button>
+          <Button className="mt-2">Button</Button>
+
         </div>
         <div className="font-mono text-xs text-muted-foreground">
           (Press <kbd>d</kbd> to toggle dark mode  sds)
